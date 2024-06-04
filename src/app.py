fc34923211dc6c606755c18ea664165fe982b86b
shirleyanan <43081884+shirleyanan@users.noreply.github.com>
2024-06-04 09:47:46 +0800

    Update app.py
    Signed-off-by: shirleyanan <43081884+shirleyanan@users.noreply.github.com>

--- src/app.py
+++ src/app.py
@@ -4,21 +4,26 @@ import pickle
 import cv2 as cv
 import numpy as np
 from glob import glob
-from .vid import proc_video, VideoProcessorCV
-from .utils import create_board_object_pts, save_points, load_points, \
-    save_camera, load_camera, load_manual_points, load_dlc_points_as_df, \
-    find_scene_file, save_optimised_cheetah, save_3d_cheetah_as_2d
-from .sba import _sba_board_points, _sba_points,_soft_sba_points
-from .calib import calibrate_camera, calibrate_fisheye_camera, \
-    calibrate_pair_extrinsics, calibrate_pair_extrinsics_fisheye, \
-    create_undistort_point_function, create_undistort_fisheye_point_function, \
-    triangulate_points, triangulate_points_fisheye, \
-    project_points, project_points_fisheye, \
-    _calibrate_pairwise_extrinsics
+from .utils import save_points, load_points, load_camera, load_manual_points,find_scene_file
+from .calib import create_undistort_point_function, create_undistort_fisheye_point_function, \
+ triangulate_points_fisheye, project_points_fisheye
 from .plotting import plot_calib_board, plot_optimized_states, \
     plot_extrinsics, Cheetah
 from .points import find_corners_images
 
+
+def extract_corners_from_images(img_dir, out_fpath, board_shape, board_edge_len, window_size=11, remove_unused_images=False):
+    print(f"Finding calibration board corners for images in {img_dir}")
+    filepaths = sorted([os.path.join(img_dir, fname) for fname in os.listdir(img_dir) if fname.endswith(".jpg") or fname.endswith(".png")])
+    points, fpaths, cam_res = find_corners_images(filepaths, board_shape, window_size=window_size)
+    saved_fnames = [os.path.basename(f) for f in fpaths]
+    saved_points = points.tolist()
+    if remove_unused_images:
+        for f in filepaths:
+            if os.path.basename(f) not in saved_fnames:
+                print(f"Removing {f}")
+                os.remove(f)
+    save_points(out_fpath, saved_points, saved_fnames, board_shape, board_edge_len, cam_res)
 # ==========  PLOTTING  ==========
 
 def plot_corners(points_fpath):
@@ -93,49 +98,3 @@ def plot_multiple_cheetah_reconstructions(data_fpaths, scene_fname=None, **kwarg
         positions.append(data["smoothed_positions"] if 'EKF' in label else data["positions"])
         labels.append(label)
     _plot_cheetah_reconstruction(positions, os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(data_fpath)))), scene_fname, labels, **kwargs)
-
-
-# ==========  VIDS  ==========
-
-def get_vid_info(path_dir, vid_extension='mp4'):
-    """Finds a video specified in/by the path variable and returns its info
-    
-    :param path: Either a directory containing a video or the path to a specific video file
-    """
-    from errno import ENOENT
-    
-    orig_path = path_dir
-    if not os.path.isfile(path_dir):
-        files = sorted(glob(os.path.join(path_dir, f"*.{vid_extension}"))) # assume path is a dir that holds video file(s)
-        if files:
-            path_dir = files[0]
-        else:
-            raise FileNotFoundError(ENOENT, os.strerror(ENOENT), orig_path) # assume videos didn't open due to incorrect path
-        
-    vid = VideoProcessorCV(in_name=path_dir)
-    vid.close()
-    return (vid.width(), vid.height()), vid.fps(), vid.frame_count(), vid.codec()
-
-
-def create_labeled_videos(video_fpaths, videotype="mp4", codec="mp4v", outputframerate=None, out_dir=None, draw_skeleton=False, pcutoff=0.5, dotsize=6, colormap='jet', skeleton_color='white'):
-    from functools import partial
-    from multiprocessing import Pool
-
-    print('Saving labeled videos...')
-    
-    bodyparts = get_markers()
-    bodyparts2connect = get_skeleton() if draw_skeleton else None
-
-    if not video_fpaths:
-        print("No videos were found. Please check your paths\n")
-        return
-
-    if out_dir is None:
-        out_dir = os.path.relpath(os.path.dirname(video_fpaths[0]), os.getcwd())
-
-    func = partial(proc_video, out_dir, bodyparts, codec, bodyparts2connect, outputframerate, draw_skeleton, pcutoff, dotsize, colormap, skeleton_color)
-
-    with Pool(min(os.cpu_count(), len(video_fpaths))) as pool:
-        pool.map(func,video_fpaths)
-        
-    print('Done!\n')
